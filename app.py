b5691971ccb44e9b9ec12c15a204c7ca1dbbe805
mercedesbenz00 <sony010521@gmail.com>
2017-06-25 14:13:32 -0400

    Update error handlers and templates.

--- app.py
+++ app.py
@@ -273,6 +273,20 @@ def get_stats():
     return stats
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template(
+        '404.html'
+    ), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template(
+        '500.html'
+    ), 500
+
+
 @app.template_filter('datetime')
 def convert_unixtime(unixtime, format='medium'):
     n = datetime.fromtimestamp(
